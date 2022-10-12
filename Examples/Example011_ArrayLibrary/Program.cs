2b77bbcd94f19420d67cc9219b98760294aabe76
Sergey <sergey198891@mail.ru>
2022-10-12 12:47:58 +0300

    added examples from lection

---
+++ Examples/Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,50 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // Если искомого элемента не будет в массиве, то вернется position равная -1, что будет означать, что элемента в массиве нет.
+    //Если оставить position = 0, то тогда, если искомого элемента в массиве нет, то у position будет возвращаться значение 0, что не верно.
+
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
